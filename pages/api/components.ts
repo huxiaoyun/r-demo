bee6eb6652a837102c394e1e55371cd0c1a3d352
evilucifero <x@evilucifero.com>
2017-11-16 16:44:59 +0800

    feat: add guide installation and update homepage

--- pages/api/components.ts
+++ pages/api/components.ts
@@ -1,7 +1,12 @@
+const Installation = require('./Guide/Installation/Installation.md').default;
+
 const Demo = require('./DemoFolder/Demo/Demo.md').default;
 const Demo1 = require('./DemoFolder/Demo1/Demo.md').default;
 
 const components = {
+  Guide: {
+    Installation,
+  },
   DemoFolder: {
     Demo: Demo,
     Demo1: Demo1,
